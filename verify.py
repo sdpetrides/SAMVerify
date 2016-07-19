cabfbde9b44666cf2302ab74b2c514f7c513c7e5
Stephen Petrides <sdpetrides@gmail.com>
2016-07-19 12:40:38 -0400

    Modular functions; use of whole genome FASTA enabled

--- verify.py
+++ verify.py
@@ -3,25 +3,61 @@
 
 import sys
 import json
+import math
 
 ## Print Reference ##
 
-def find_in_ref(sam_line, ref_dir_str):
-	chromosome = sam_line[2]
-	raw_seq = sam_line[9]
-	chromosome_str = ref_dir_str + '/' + chromosome + '.fa'
-	chromosome_file = open(chromosome_str, 'rt')
-	position = int(sam_line[3])
-	seq_len = len(raw_seq)
+def find_in_ref(chromosome_str, position_str, seq_len_str, ref_str, ref_str_idx='.fai', rev_comp=False):
+	""""""
+	chr_len = len(chromosome_str)
+	pos_int = int(position_str)
+	seq_len = int(seq_len_str)
+	if ref_str_idx == '.fai':
+		ref_str_idx = ref_str + ref_str_idx
+	elif ref_str_idx == False:
+		rev_comp == False
+		ref_str_idx = ref_str + '.fai'
 
-	chromosome_file_str = chromosome_file.read().replace('\n', '').upper()
-	ref_seq = chromosome_file_str[len(chromosome)+position:len(chromosome)+position+seq_len]
+	if ref_str.endswith('.fa') or ref_str.endswith('.fasta'):
+		genome_file = open(ref_str, 'rt')
 
-	print("Reference:\t" + ref_seq)
+		with open(ref_str_idx, 'rt') as index_file:
+			index_lines = index_file.readlines()
+		for line in index_lines:
+			line_list = line.split()
+			if chromosome_str == line_list[0]:
+				offset = int(line_list[2])
+				whole_chr_len = int(line_list[1])
+				updated_pos = pos_int + math.floor(pos_int/int(line_list[3]))
+
+		genome_file.seek(offset+updated_pos-1, 0)
+		ref_seq = genome_file.read(seq_len+2).replace('\n', '').upper()
+		if len(ref_seq) != seq_len:
+			ref_seq = ref_seq[:-1]
+		genome_file.close()
+	else:
+		chromosome_str = ref_str + '/' + chromosome_str + '.fa'
+		chromosome_file = open(chromosome_str, 'rt')
+		chromosome_file_str = chromosome_file.read().replace('\n', '').upper()
+		ref_seq = chromosome_file_str[chr_len+pos_int:chr_len+pos_int+seq_len]
+		chromosome_file.close()
+
+	if rev_comp is True:
+		ref_seq = reverse_complement(ref_seq)
+		return ref_seq
+	else:
+		return ref_seq
 
 ## Reconstruct Reference ##
 
-def reconstruct(sam_line):
+def reconstruct(line):
+	""""""
+	sam_line = line.split()
+
+	flag_str = str(bin(int(sam_line[1])))
+	if flag_str[-3] == '1':
+		return 'Read unmapped'
+
 	reconstruct_str = sam_line[9]
 	cigar_str = sam_line[5]
 	for field in sam_line:
@@ -60,9 +96,26 @@ def reconstruct(sam_line):
 			else:
 				count+=1
 				reconstruct_str = reconstruct_str[0:SEQ_pos] + md_str[count].lower() + reconstruct_str[SEQ_pos:]
-	print('Reconstruction:\t' + reconstruct_str)
+	return reconstruct_str
 
-
+## Reverse Complement Sequence ##
+def reverse_complement(seq):
+	""""""
+	seq = seq.upper()
+	qes = ''
+	for base in seq:
+		if base is 'A':
+			qes+='T'
+		elif base is 'T':
+			qes+='A'
+		elif base is 'C':
+			qes+='G'
+		elif base is 'G':
+			qes+='C'
+		else:
+			return 'Error: Incorrect bases included in sequence. Use only ATCG.'
+	qes = qes[::-1]
+	return qes
 
 
 #### MAIN ####
@@ -75,9 +128,11 @@ def main():
 
 	manual = ["\nCommands and Options", "\n\n\tBasic Command Structure:", "\n\n\t\t$ python3 verify.py <flag> <path>",
 	"\n\t\t\t# Flags can come in any order but must be followed by corresponding information",
-	"\n\tFlags", "\n\t\t-man", "\n\t\t\t# Prints Commands and Options section of MANUAL.", "\n\t\t-sam",
-	"\n\t\t\t# Followed by the path/to/file_name.sam", "\n\t\t-ref_dir",
-	"\n\t\t\t# Followed by the path to the directory containing chr_.fa", "\n\t\t-header (optional)",
+	"\n\tFlags", "\n\t\t-man", "\n\t\t\t# Prints Commands and Options section of MANUAL.", "\n\t\t-sam (required)",
+	"\n\t\t\t# Followed by the path/to/file_name.sam", "\n\t\t-ref (required)",
+	"\n\t\t\t# Either a path to a whole genome.fa or a path to directory containing chr_.fa files", "\n\t\t-ref_idx (optional)"
+	"\n\t\t\t# Use if whole genome is used and .fai is in different directory or ",
+	"\n\t\t\t with difference file name than [whole_genome_file.fa].fai", "\n\t\t-header (optional)",
 	"\n\t\t\t# Followed by the header of target read", "\n\t\t\t# If no header is given the first read is verified"]
 
 	while param_count <= argc-1:
@@ -95,17 +150,20 @@ def main():
 				param_str = sys.argv[param_count]
 				param_count +=1
 				sam_str = param_str
-			elif param_str == '-ref_dir':
+			elif param_str == '-ref':
 				param_str = sys.argv[param_count]
 				param_count +=1
-				ref_dir_str = param_str
+				ref_str = param_str
+			elif param_str == '-ref_idx':
+				param_str = sys.argv[param_count]
+				param_count +=1
+				ref_idx = param_str
 			elif param_str == '-header':
 				param_str = sys.argv[param_count]
 				param_count +=1
 				header_str = param_str
 
-## Print Sequence From SAM ##
-
+   ## Print Sequence From SAM ##
 	sam_file = open(sam_str, 'rt')
 
 	if '-header' in param_set:
@@ -113,14 +171,21 @@ def main():
 		for line in sam_file:
 			sam_line = line.split()
 			if sam_line[0] == header_str:
+				flag_str = str(bin(int(sam_line[1])))
+				if flag_str[-3] == '1':
+					print('Read is unmapped.')
+					continue;
 				position = sam_line[3]
 				raw_seq = sam_line[9]
 				print('Position:\t' + position + '\t' + sam_line[2] + '\t' + sam_line[11])
 				print('Read:\t\t' + raw_seq)
 
-				if '-ref_dir' in param_set:
-					find_in_ref(sam_line, ref_dir_str)			# call function to print from reference
-					reconstruct(sam_line)						# call function to reconstruct from CIGAR and TAGs
+				if '-ref' in param_set and '-ref_idx' in param_set:
+					print('Reference:\t' + find_in_ref(sam_line[2], sam_line[3], len(sam_line[9]), ref_str, ref_idx))
+					print('Reconstruction:\t' + reconstruct(line))
+				elif '-ref' in param_set:
+					print('Reference:\t' + find_in_ref(sam_line[2], sam_line[3], len(sam_line[9]), ref_str))
+					print('Reconstruction:\t' + reconstruct(line))	
 				else: 
 					print("Parameters not entered correctly. See MANUAL for proper syntax.\nError: No reference genome specified.")
 
@@ -129,17 +194,25 @@ def main():
 			sam_line = line.split()
 			first_word = sam_line[0]
 			if first_word[0] != '@':
+				flag_str = str(bin(int(sam_line[1])))
+				if flag_str[-3] == '1':
+					continue;
 				print('Header:\t\t' + first_word)
 				position = sam_line[3]
 				raw_seq = sam_line[9]
 				print('Position:\t' + position + '\t' + sam_line[2])
 				print('Read:\t\t' + raw_seq)
-				if '-ref_dir' in param_set:
-					find_in_ref(sam_line, ref_dir_str)
-					reconstruct(sam_line)
+				if '-ref' in param_set and '-ref_idx' in param_set:
+					print('Reference:\t' + find_in_ref(sam_line[2], sam_line[3], len(sam_line[9]), ref_str, ref_idx))
+					print('Reconstruction:\t' + reconstruct(line))
+					break;
+				elif '-ref' in param_set:
+					print('Reference:\t' + find_in_ref(sam_line[2], sam_line[3], len(sam_line[9]), ref_str))
+					print('Reconstruction:\t' + reconstruct(line))
 					break;
 				else:
 					print("Parameters not entered correctly. See MANUAL for proper syntax.\nError: No reference genome specified.")	
+	sam_file.close()
 
 if __name__ == "__main__":
     main()
